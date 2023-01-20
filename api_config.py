bc6ea55d2bd13ef1c68952516114f47896186bbf
Eric Callaway <ericcallaway5@gmail.com>
2023-01-20 14:04:50 -0600

    [ADD] Can now receive data from spotify given a playlist ID.

--- api_config.py
+++ api_config.py
@@ -19,32 +19,72 @@ url = "https://accounts.spotify.com/api/token"
 headers = {}
 data = {}
 
+def getAccessToken(clientID: str, clientSecret: str) ->str:
+    #Endcode as Base64
+    message = f"{clientID}:{clientSecret}"
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
 
-#Endcode as Base64
-message = f"{clientID}:{clientSecret}"
-message_bytes = message.encode('ascii')
-base64_bytes = base64.b64encode(message_bytes)
-base64_message = base64_bytes.decode('ascii')
+
+
+    token_data = {
+        'grant_type' : 'client_credentials'
+    }
+
+    token_header = {
+        'Authorization' : "Basic " + base64_message #<base64 encoded cid:secret>
+    }
+
+    #Formal request for an access token from spotify
+    res = requests.post(url, headers=token_header, data=token_data)
+
+    #json object received from Spotify
+    responseObject = res.json()
+
+    # print(json.dumps(responseObject, indent=2))
+
+    #Extracting the actual access token from json object received from Spotify
+    accessToken = responseObject['access_token']
+
+    return accessToken
+
+def getPlaylistTracks(token: str, playlistID: str):
+    playlistEndPoint = f"https://api.spotify.com/v1/playlists/{playlistID}"
+
+    getHeader = {
+        "Authorization": "Bearer " + token
+    }
+
+    res = requests.get(playlistEndPoint, headers=getHeader)
+
+    playlistObj = res.json()
+
+    return playlistObj
+
+
+#API requests 
+token = getAccessToken(clientID, clientSecret)
+playlistID = '37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f&nd=1'
+
+tracklist = getPlaylistTracks(token, playlistID)
+
+#Writes the complete json object into a json file
+with open('tracklist.json', 'w') as f:
+    json.dump(tracklist, f)
+
+
+#Returns the names of the tracks in the json object
+for t in tracklist['tracks']['items']:
+    songName = t['track']['name']
+    print(songName)
+
+for u in tracklist['tracks']['items']:
+    songURI = u['track']['uri']
+    print(songURI)
 
 
 
-token_data = {
-    'grant_type' : 'client_credentials'
-}
 
-token_header = {
-    'Authorization' : "Basic " + base64_message #<base64 encoded cid:secret>
-}
-
-#Formal request for an access token from spotify
-res = requests.post(url, headers=token_header, data=token_data)
-
-#json object received from Spotify
-responseObject = res.json()
-
-# print(json.dumps(responseObject, indent=2))
-
-#Extracting the actual access token from json object received from Spotify
-accessToken = responseObject['access_token']
 
 
