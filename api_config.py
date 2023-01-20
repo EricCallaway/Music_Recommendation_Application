de18950b58711aa0aeab7b780fda5d43d57fc4bf
Eric Callaway <ericcallaway5@gmail.com>
2023-01-19 20:43:44 -0600

    [ADD] The client credentials are now being read. Access token is being received.

--- api_config.py
+++ api_config.py
@@ -1,11 +1,50 @@
 import spotipy
+import requests
+import datetime
+import base64
+import json
 from spotipy.oauth2 import SpotifyClientCredentials
+from secret import *
 
-with open("secret.txt") as f:
-    secret_ls = f.readlines()
-    cid = secret_ls[0][:-2]
-    secret = secret_ls[1]
+# with open("secret.txt") as f:
+#     secret_ls = f.readlines()
+#     cid = secret_ls[0][:-2]
+#     secret = secret_ls[1]
 
 #Authentication to Spotify API without specific user credentials
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+# client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+# sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+url = "https://accounts.spotify.com/api/token"
+headers = {}
+data = {}
+
+
+#Endcode as Base64
+message = f"{clientID}:{clientSecret}"
+message_bytes = message.encode('ascii')
+base64_bytes = base64.b64encode(message_bytes)
+base64_message = base64_bytes.decode('ascii')
+
+
+
+token_data = {
+    'grant_type' : 'client_credentials'
+}
+
+token_header = {
+    'Authorization' : "Basic " + base64_message #<base64 encoded cid:secret>
+}
+
+#Formal request for an access token from spotify
+res = requests.post(url, headers=token_header, data=token_data)
+
+#json object received from Spotify
+responseObject = res.json()
+
+# print(json.dumps(responseObject, indent=2))
+
+#Extracting the actual access token from json object received from Spotify
+accessToken = responseObject['access_token']
+
+
---
+++ secret.py
@@ -0,0 +1,2 @@
+clientID = "d0122bec0c8c4f7880c2c4027ab70e37"
+clientSecret = "1edf54562e5647029fad3405bb9618c3"
