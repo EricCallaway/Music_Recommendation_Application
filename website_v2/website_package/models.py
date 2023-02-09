74f6b8406b9cb48f9e4e1d150f4301c332ba6d70
Eric Callaway <ericcallaway5@gmail.com>
2023-02-09 09:56:31 -0600

    [REF] Refactoring. git commit -m[REF] Refactoring.

--- website_v2/website_package/models.py
+++ website_v2/website_package/models.py
@@ -32,5 +32,12 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
     age = db.Column(db.String(150))
+
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'age' : self.age
+        }
     
         
